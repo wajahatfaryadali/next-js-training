13219c11cd198a60f56bbee78fe76ee2f6724ab3
Wajahat Ali <wajahatfaryadali@gmail.com>
2024-02-11 15:14:30 +0500

    Contact form added

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,8 +1,46 @@
 import React from 'react'
+import Image from 'next/image'
+import contact from '../../assets/pngs/contact.png'
+import classes from './contact.module.css'
 
 function Contact() {
+
+  const stats = [
+    {
+      title: 'Year of Experience',
+      count: '10 k+'
+    },
+    {
+      title: 'Services and Plugins',
+      count: '101 k+'
+    },
+    {
+      title: 'Happy clients',
+      count: '102 k+'
+    }
+  ]
+
   return (
-    <div>Contact</div>
+    <main className={classes.mainContainer}>
+      <div className={classes.imgContainer}>
+        <Image src={contact} alt="hero-image" className={classes.heroImg} />
+      </div>
+      <div className={classes.formContainer}>
+        <form action="" className={classes.form}>
+          <input type="text" id="name" name="name" placeholder='Name and Surename' />
+          <input type="email" id="email" name="email" placeholder='Email address' />
+          <input type="text" id="phone" name="phone" placeholder='Phone number (optional)' />
+          <textarea
+            name=''
+            id=''
+            rows={10}
+            cols={30}
+            placeholder='Message'
+          />
+          <button type='submit' className={classes.submitBtn}>Submit</button>
+        </form>
+      </div>
+    </main>
   )
 }
 
